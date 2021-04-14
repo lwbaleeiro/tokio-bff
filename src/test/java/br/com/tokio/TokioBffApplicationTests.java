80e5b4fc4f622dcc42a835ea9cf65cfb4d3151df
unknown <lw_baleeiro@hotmail.com>
2021-04-14 10:18:07 -0300

    projeto inicial

---
+++ src/test/java/br/com/tokio/TokioBffApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tokio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokioBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
